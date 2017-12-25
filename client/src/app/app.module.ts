c4e3aec9355484f4b3fa708a20291f39da4696d6
Rick Li <ricklihs@hotmail.com>
2017-12-25 06:48:04 -0800

    add files folders

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,17 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AlertComponent } from './_directives/alert.component';
+import { AlertService } from './_services/alert.service';
+import { AuthenticationService } from './_services/authentication.service';
+import { UserService } from './_services/user.service';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [AlertService, AuthenticationService, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
